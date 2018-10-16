a59c5f978c851fd704dcc927295af6e3f9d6f81e
JB Plizga <bradplizga@gmail.com>
2018-10-16 17:38:18 -0400

    Add customization and home page activities

---
+++ app/src/main/java/com/example/labs/myapplication/CustomizationMenu.java
@@ -0,0 +1,13 @@
+package com.example.labs.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomizationMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customization_menu);
+    }
+}
